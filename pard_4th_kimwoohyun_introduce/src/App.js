defb6a935ecb362bb53ffbb0fb352b7fab16755f
Kim woo hyun <151426184+8oo30un@users.noreply.github.com>
2024-09-08 18:35:00 +0900

    add: name and pic - assign1

--- pard_4th_kimwoohyun_introduce/src/App.js
+++ pard_4th_kimwoohyun_introduce/src/App.js
@@ -1,22 +1,25 @@
-import logo from './logo.svg';
+import snowImage from './snow.jpeg'
+import rowImage from './row.jpeg'
 import './App.css';
-
+//onClink 함수를 쓸때는 useState 를 써야함으로 지금 구현은 힘들듯하다
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="Name">Kim Woo hyun</h1>
+        <p className="format">pard 4th web part <br></br>
+          assignment1 - introduce</p>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <button type="button" className="button" onClick="scrollToSection()">name</button>
+          <p>김우현</p> <br></br>
+          <button type="button" className="button">picture</button>
+          <p>
+            <img src={snowImage} className="Image" />
+            <img src={rowImage} className="Image" />
+          </p>
+          <button type="button" className="button">aim</button>
+          <button type="button" className="button">fav.3</button>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
