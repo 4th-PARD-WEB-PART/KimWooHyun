71ebc10bb3411481118a7eadd1c9492d48ea36f9
Kim woo hyun <151426184+8oo30un@users.noreply.github.com>
2024-09-12 20:53:52 +0900

    finish : assign1

--- pard_4th_kimwoohyun_introduce/src/App.js
+++ pard_4th_kimwoohyun_introduce/src/App.js
@@ -1,5 +1,6 @@
 import snowImage from './snow.jpeg'
 import rowImage from './row.jpeg'
+import albumImage from './album.jpg'
 import './App.css';
 //onClink 함수를 쓸때는 useState 를 써야함으로 지금 구현은 힘들듯하다
 function App() {
@@ -24,12 +25,16 @@ function App() {
             <img src={rowImage} className="rowImage" />
             <br></br>
             Club - Armstrong<br></br>
+            <br/>
+            <img src={albumImage} className = "albumImage"/>
+            <br></br>
+            Favorite artist - ph-1
             </p>
           <button type="button" className="button">aim</button>
           <p>
             2024 aims <br/> <br/>
             1. 구체적인 전공 알아보기 in computer science<br/>
-            2. 달리기 - NIKE Run club greem level<br/>
+            <p className="green"> 2. 달리기 - NIKE Run club green level</p>
             3. 성격 바꾸기 - I에서 E, P에서 J
           </p>
           <button type="button" className="button">fav.3</button>
